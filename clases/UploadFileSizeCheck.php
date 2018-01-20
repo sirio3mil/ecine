8e2e49bd47bf1301ba45e1ab8decdb6dd8a780a6
Reynier de la Rosa <sirio3mil@gmail.com>
2018-01-21 00:26:53 +0100

    val as number

--- clases/UploadFileSizeCheck.php
+++ clases/UploadFileSizeCheck.php
@@ -9,6 +9,7 @@ class UploadFileSizeCheck{
 	public static function getBytes($val){
 		$val = trim($val);
 		$last = strtolower($val[strlen($val) - 1]);
+		$val = substr($val, 0, -1);
 		switch($last){
 			// The 'G' modifier is available since PHP 5.1.0
 			case 'g':
