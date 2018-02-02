d4ed28c1d4c90d5f35eb4dcc92d19ac159bd1988
Reynier de la Rosa <sirio3mil@gmail.com>
2018-02-02 21:50:33 +0100

    updated navbar style to bootstrap 4

--- index.php
+++ index.php
@@ -25,77 +25,69 @@ $open_page = (empty($open_page))?'series':$open_page;
 </head>
 <body>
 	<div class="container-fluid margin-top-10">
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<form name="frmbusqueda" method="post" role="search" class="navbar-form navbar-left" action="redirect_busqueda.php">
-				<div class="form-group">
-					<input placeholder="buscar" class="form-control" name="condicion" type="text" value="<?=(!empty($_GET['cadena']))?urldecode($_GET['cadena']):""?>" />
-				</div>
-				<button type="submit" class="btn btn-default">Buscar</button>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<form method="post" role="search" class="form-inline my-2 my-lg-0" action="redirect_busqueda.php">
+				<input placeholder="buscar" class="form-control mr-sm-2" name="condicion" type="text" value="<?=(!empty($_GET['cadena']))?urldecode($_GET['cadena']):""?>" />
+				<button type="submit" class="btn btn-outline-success my-2 my-sm-0">Buscar</button>
 			</form>
-			<div class="navbar-form navbar-left">
-				<div class="form-group">
-					<input placeholder="id" class="form-control" type="text" id="id-avanzar-pelicula" />
-				</div>
-				<button type="button" class="btn btn-default" id="button-avanzar-pelicula">Ir</button>
-			</div>
-			<form name="formulario_antiguo_enviar" method="post" role="search" class="navbar-form navbar-left" action="index.php?page=extraer">
-				<div class="form-group">
-					<input name="pagina" placeholder="url a examinar" id="pagina_default" type="text" size="60" class="form-control" />
-				</div>
-				<button type="submit" class="btn btn-default">Importar</button>
+            <form method="post" role="search" class="form-inline my-2 my-lg-0">
+				<input placeholder="id" class="form-control mr-sm-2" type="text" id="id-avanzar-pelicula" />
+				<button type="button" class="btn btn-default my-2 my-sm-0" id="button-avanzar-pelicula">Ir</button>
+            </form>
+			<form method="post" role="search" class="form-inline my-2 my-lg-0" action="index.php?page=extraer">
+				<input name="pagina" placeholder="url a examinar" id="pagina_default" type="text" size="60" class="form-control mr-sm-2" />
+				<button type="submit" class="btn btn-default my-2 my-sm-0">Importar</button>
 			</form>
-			<div class="navbar-form navbar-left">
-				<div class="btn-group" role="group" aria-label="...">
-					<button type="button" id="button-avanzar-anterior" class="btn btn-default">
+            <form class="form-inline">
+					<button type="button" id="button-avanzar-anterior" class="btn btn-sm btn-outline-secondary">
 						<i class="fa fa-step-backward" style="padding: 3px 0" aria-hidden="true"></i>
 					</button>
-					<button type="button" id="button-avanzar-siguiente" class="btn btn-default">
+					<button type="button" id="button-avanzar-siguiente" class="btn btn-sm btn-outline-secondary">
 						<i class="fa fa-step-forward" style="padding: 3px 0" aria-hidden="true"></i>
 					</button>
-					<button type="button" id="button-avanzar-ultimo" class="btn btn-default">
+					<button type="button" id="button-avanzar-ultimo" class="btn btn-sm btn-outline-secondary">
 						<i class="fa fa-fast-forward" style="padding: 3px 0" aria-hidden="true"></i>
 					</button>
-					<button type="button" id="button-avanzar-nuevo" class="btn btn-default">
+					<button type="button" id="button-avanzar-nuevo" class="btn btn-sm btn-outline-secondary">
 						<i class="fa fa-asterisk" style="padding: 3px 0" aria-hidden="true"></i>
 					</button>
-				</div>
-				<div class="btn-group">
-					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+				<div class="dropdown">
+					<button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown">
 						Insertar información <span class="caret"></span>
 					</button>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href='#' class='contenido' data-page='nuevo_registro' data-table='filmes_titulos_adicionales'>Otítulo</a></li>
-						<li><a href='#' class='contenido' data-page='nuevo_registro' data-table='filmes_fechas_estreno'>Estreno</a></li>
-						<li><a href='#' class='contenido' data-page='nuevo_registro' data-table='filmes_idiomas'>Idioma</a></li>
-						<li><a href='#' class='contenido' data-page='nuevo_registro' data-table='filmes_paises'>Pais</a></li>
-						<li><a href='#' class='contenido' data-page='nuevo_registro' data-table='filmes_generos'>Genero</a></li>
-						<li><a href="index.php?page=lotes" title="Agregar usuarios separados por comas a la película">Actores</a></li>
-						<li><a href='#' class='contenido' data-page='ventana_cert'>Clasificación</a></li>
-						<li><a href='#' class='contenido' data-page='ventana_estudios'>Distribuidor</a></li>
-						<li><a href='#' class='contenido' data-page='ventana_filmes'>Recomendada</a></li>
-						<li><a href="actualiza_votos.php">Actualizar Votos</a></li>
-					</ul>
+                    <div class="dropdown-menu">
+						<a href='#' class="dropdown-item" data-page='nuevo_registro' data-table='filmes_titulos_adicionales'>Otítulo</a>
+						<a href='#' class="dropdown-item" data-page='nuevo_registro' data-table='filmes_fechas_estreno'>Estreno</a>
+						<a href='#' class="dropdown-item" data-page='nuevo_registro' data-table='filmes_idiomas'>Idioma</a>
+						<a href='#' class="dropdown-item" data-page='nuevo_registro' data-table='filmes_paises'>Pais</a>
+						<a href='#' class="dropdown-item" data-page='nuevo_registro' data-table='filmes_generos'>Genero</a>
+						<a class="dropdown-item" href="index.php?page=lotes" title="Agregar usuarios separados por comas a la película">Actores</a>
+						<a href='#' class="dropdown-item" data-page='ventana_cert'>Clasificación</a>
+						<a href='#' class="dropdown-item" data-page='ventana_estudios'>Distribuidor</a>
+						<a href='#' class="dropdown-item" data-page='ventana_filmes'>Recomendada</a>
+						<a class="dropdown-item" href="actualiza_votos.php">Actualizar Votos</a>
+					</div>
 				</div>
-				<div class="btn-group">
-					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+				<div class="dropdown">
+					<button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle" data-toggle="dropdown">
 						Listados <span class="caret"></span>
 					</button>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="index.php?page=series" title="series que sigo">Series que sigo</a></li>
-						<li><a href="index.php?page=pendientes" title="películas pendientes de ver">Películas disponibles</a></li>
+                    <div class="dropdown-menu">
+						<a class="dropdown-item" href="index.php?page=series" title="series que sigo">Series que sigo</a>
+						<a class="dropdown-item" href="index.php?page=pendientes" title="películas pendientes de ver">Películas disponibles</a>
 						<?php
 						$query = "SELECT listado_id, codigo FROM listados WHERE anulado = 0";
 						$result = $mysqli->query($query);
 						while($row = $result->fetch_object()){
-							printf("<li><a href='index.php?page=listados&listado=%u'>%s</a></li>", $row->listado_id, $row->codigo);
+							printf("<a class=\"dropdown-item\" href='index.php?page=listados&listado=%u'>%s</a>", $row->listado_id, $row->codigo);
 						}
 						$result->close();
 						?>
-						<li><a href="index.php?page=traducciones" title="Muestra los paises, idiomas y géneros pendientes de traducir">Translations</a></li>
-					</ul>
+						<a class="dropdown-item" href="index.php?page=traducciones" title="Muestra los paises, idiomas y géneros pendientes de traducir">Translations</a>
+					</div>
 				</div>
-			</div>
-		</div>
+			</form>
+		</nav>
 	</div>
 	<div class="container-fluid margin-top-10">
 		<div class="row">
