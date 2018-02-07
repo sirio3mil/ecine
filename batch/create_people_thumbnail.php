7c06821629fdd701e43b5aba663f39163dc858d4
root <root@ecine-nginx.c.hallowed-object-191920.internal>
2018-02-07 17:20:21 +0000

    fixed directory level

--- batch/create_people_thumbnail.php
+++ batch/create_people_thumbnail.php
@@ -11,8 +11,8 @@ $query = "select id from actores where cover = 1";
 $result = $mysqli->query($query);
 if ($result) {
     while ($row = $result->fetch_object()){
-        $original_path = sprintf("%s%sphotos%sactores%soriginal%s%u.jpg", dirname(__FILE__, 1), DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $row->id);
-        $target_path = sprintf("%s%sphotos%sactores%s40%s%u.jpg", dirname(__FILE__, 1), DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $row->id);
+        $original_path = sprintf("%s%sphotos%sactores%soriginal%s%u.jpg", dirname(__FILE__, 2), DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $row->id);
+        $target_path = sprintf("%s%sphotos%sactores%s40%s%u.jpg", dirname(__FILE__, 2), DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $row->id);
         $image = imagecreatefromjpeg($original_path);
         PhotoThumbnail::create($target_path, $image, 40);
     }
